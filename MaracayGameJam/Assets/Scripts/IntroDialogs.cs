91b3d9153313aa7d3ed6f89ee4c40a0fd73157ab
David Pino <aesthezel@gmail.com>
2021-01-31 14:01:28 -0400

    Actualizado posiciones de objetos

---
+++ MaracayGameJam/Assets/Scripts/IntroDialogs.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroDialogs : MonoBehaviour
+{
+    [SerializeField] private string[] dialogs;
+    [SerializeField] private Text currentDialog;
+    private int actualDialog = 0;
+    private Animator animator;
+
+    #region Class Logic
+    public void NextDialog()
+    {
+        if(actualDialog <= dialogs.Length)
+        {
+            actualDialog++;
+        }
+        currentDialog.text = dialogs[actualDialog];
+    }
+    #endregion
+
+    #region MonoBehaviour API
+    private void Awake() 
+    {
+        animator = GetComponent<Animator>();
+    }
+
+
+    private void Start()
+    {
+        actualDialog = 0;
+        currentDialog.text = dialogs[actualDialog];
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion
+}
