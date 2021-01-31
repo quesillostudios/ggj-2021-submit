d0be318231d198c475c9e1091c9f8c17984d4f7d
David Pino <aesthezel@gmail.com>
2021-01-31 14:50:14 -0400

    Nueva UI

---
+++ MaracayGameJam/Assets/Scripts/Rune.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Rune : MonoBehaviour
+{
+    #region Class Logic
+    #endregion
+
+    #region MonoBehaviour API
+    private void OnCollisionEnter(Collision other) 
+    {
+        LimboManager.Instance.AddGameScore();
+        Destroy(this.gameObject);
+    }
+    #endregion
+}
