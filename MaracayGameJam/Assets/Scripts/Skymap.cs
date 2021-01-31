465b190044c5c97a5e944bf26c06e0f6047da23a
David Pino <aesthezel@gmail.com>
2021-01-31 09:09:36 -0400

    Implementando mejoras gráficas
    Agregado el skymap

---
+++ MaracayGameJam/Assets/Scripts/Skymap.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class Skymap : MonoBehaviour
+{
+    #region Class Logic
+    #endregion
+
+    #region MonoBehaviour API
+    private void Awake()
+    {
+        
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+    #endregion
+}
